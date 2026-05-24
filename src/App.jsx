36f16a6056a25ab8bd0f59b1013f29a2d7cddbd9
joshuaashley-web <joshua.shly1@gmail.com>
2026-05-24 01:24:23 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ export default function App() {
             className="slide"
             style={{
               backgroundImage:
-                "url(https://images.unsplash.com/photo-1513828583688-c52646db42da?auto=format&fit=crop&w=1600&q=80)",
+                "url(https://images.unsplash.com/photo-1523660778745-247ed0bcce31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
             }}
           ></div>
 
