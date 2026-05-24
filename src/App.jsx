61a617825b6e29aae6996fed0e1361b0576dca37
joshuaashley-web <joshua.shly1@gmail.com>
2026-05-24 01:28:45 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,7 @@ export default function App() {
 
         <div style={gridStyle}>
           <img
-            src="https://images.unsplash.com/photo-1513828583688-c52646db42da?auto=format&fit=crop&w=900&q=80"
+            src="(https://images.unsplash.com/photo-1523660778745-247ed0bcce31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
             alt="Mining tools"
             style={imageStyle}
           />
